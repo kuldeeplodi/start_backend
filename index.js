fde6ed3f6cc6724c605d4b4b832318b20d50f725
kuldeeplodi <kuldeeplodi@gmail.com>
2024-10-10 20:21:29 +0530

    git

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, resp) => {
+  resp.send("hello world");
+});
+app.get("/login", (req, resp) => {
+  resp.send("<h1>please login at chai aur code");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
